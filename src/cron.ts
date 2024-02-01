fa69cf5e03b715c601c3bc7a5c50a99589b792a1
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-01 17:08:29 +0100

    remove cron

--- src/cron.ts
+++
@@ -1,9 +0,0 @@
-const cron = async () => {
-  const query = fetch('https://project-midnight-backend.onrender.com/test');
-
-  query
-    .then(res => res.json())
-    .catch(err => console.log(err));
-};
-
-cron();
