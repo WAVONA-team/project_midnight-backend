200a730d3c00c1a25dd29db394fae6469d2743b6
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-15 23:29:12 +0100

    feat(TAS-233): using both endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config.js';
 
-import { router as testRouter } from './routes/test.route.js';
+import { zodMiddleware } from './middlewares/zod.middleware.js';
+
+import { authRouter } from './routes/auth.route.js';
+import { userRouter } from './routes/user.route.js';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_HOST,
+    credentials: true,
+  }),
+);
+app.use(express.json());
 
-app.use('/test', express.json(), testRouter);
+app.use(authRouter);
+app.use('/users', userRouter);
+
+app.use(zodMiddleware);
 
 app.listen(PORT, () =>
   console.log(`Server is running on https://localhost:${PORT}`),
