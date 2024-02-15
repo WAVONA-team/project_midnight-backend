70c739c62297818ff76413fc9286f31995e80c69
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-15 23:25:22 +0100

    fix(TAS-233): remove test endpoint

--- src/routes/test.route.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import * as testController from '../controllers/test.controller.js';
-
-export const router = express.Router();
-
-router.get('/', testController.get);
--- src/services/test.service.ts
+++
@@ -1,5 +0,0 @@
-import prisma from '../client.js';
-
-export const getAll = async () => {
-  return await prisma.test.findMany();
-};
