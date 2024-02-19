39e174ce8e6fe6c0ca579f62694986f1394a0594
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-19 20:42:02 +0100

    fix(TAS-233): fix zod schemas

--- src/zodSchemas/auth/activate.ts
+++ src/zodSchemas/auth/activate.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 export const activateSchema = z.object({
   activationToken: z
     .string({
-      invalid_type_error: 'Activation token must be a string',
       required_error: 'Activation token is required',
     })
     .trim(),
--- src/zodSchemas/auth/auth.ts
+++ src/zodSchemas/auth/auth.ts
@@ -2,20 +2,20 @@ import { z } from 'zod';
 
 export const authSchema = z.object({
   email: z
-    .string({
-      required_error: 'Email is required',
-      invalid_type_error: 'Email must be a string',
-    })
+    .string()
     .trim()
+    .min(1, {
+      message: 'Email is required',
+    })
     .email({
       message: 'Invalid email address',
     }),
   password: z
-    .string({
-      required_error: 'Password is required',
-      invalid_type_error: 'Password must be a string',
-    })
+    .string()
     .trim()
+    .min(1, {
+      message: 'Password is required',
+    })
     .min(6, {
       message: 'Password should be more than 6 characters',
     }),
