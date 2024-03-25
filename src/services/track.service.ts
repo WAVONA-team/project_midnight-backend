05d8f9caa1c0de6047a21d572e8c5c3f9190adc6
Jegor volkov <jegor.volkov.a@gmail.com>
2024-03-25 11:00:14 +0100

    feat(TAS-333): refactor and add soundcloud

--- src/services/track.service.ts
+++ src/services/track.service.ts
@@ -21,36 +21,67 @@ const getYoutubeId = (url: string) => {
   return url.match(regex)?.[1] || null;
 };
 
+const getSoundCloudTrackId = (url: string) => {
+  const regex = /(?:soundcloud\.com\/\S+\/)([^/?]+)/;
+
+  return url.match(regex)?.[1] || null;
+};
+
+const checkExistingTrack = async (urlId: string, userId: string) => {
+  const track = await prisma.track.findUnique({
+    where: { userId, urlId },
+  });
+
+  const error = checkExistingTrackSchema.parse(track?.id || '');
+
+  return error;
+};
+
+const makeTrackRequest = async (url: string, urlId: string, userId: string) => {
+  return axios
+    .get<OembedTrack>(url)
+    .then(async (res) => {
+      const { title, author_name, thumbnail_url, provider_name } = res.data;
+
+      return await prisma.track.create({
+        data: {
+          title,
+          url,
+          urlId,
+          imgUrl: thumbnail_url,
+          author: author_name,
+          userId,
+          source: provider_name,
+        },
+      });
+    })
+    .catch(() => trackParsingError.parse(''));
+};
+
 const createTrack = async (url: string, userId: string) => {
   switch (true) {
     case url.includes('youtube') || url.includes('youtu.be'): {
       const urlId = getYoutubeId(url) || '';
-      const track = await prisma.track.findUnique({
-        where: { userId, urlId },
-      });
 
-      checkExistingTrackSchema.parse(track?.id || '');
+      await checkExistingTrack(urlId, userId);
 
-      return axios
-        .get<OembedTrack>(
-          `https://www.youtube.com/oembed?url=${url}&format=json`,
-        )
-        .then(async (res) => {
-          const { title, author_name, thumbnail_url, provider_name } = res.data;
+      return makeTrackRequest(
+        `https://www.youtube.com/oembed?url=${url}&format=json`,
+        urlId,
+        userId,
+      );
+    }
 
-          return await prisma.track.create({
-            data: {
-              title,
-              url,
-              urlId,
-              imgUrl: thumbnail_url,
-              author: author_name,
-              userId,
-              source: provider_name,
-            },
-          });
-        })
-        .catch(() => trackParsingError.parse(''));
+    case url.includes('soundcloud'): {
+      const urlId = getSoundCloudTrackId(url) || '';
+
+      await checkExistingTrack(urlId, userId);
+
+      return makeTrackRequest(
+        `https://soundcloud.com/oembed?format=json&url=${url}`,
+        urlId,
+        userId,
+      );
     }
 
     default: {
