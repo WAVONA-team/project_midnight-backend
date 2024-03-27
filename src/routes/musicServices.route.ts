69fc97c30eb8646d9b2e24533b6e68113f1c5779
Vo7kov <jegor.volkov.a@gmail.com>
2024-03-27 22:35:52 +0100

    refactor(TAS-334)

--- src/routes/musicServices.route.ts
+++ src/routes/musicServices.route.ts
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { type Request, type Response } from 'express';
 import { type JwtPayload } from 'jsonwebtoken';
-import { User } from 'project_midnight';
 import prisma from '../client.js';
 
 import passport from 'passport';
 import { Strategy, VerifyCallback, type Profile } from 'passport-spotify';
 
 import { jwtService } from '../services/jwt.service.js';
-import { userService } from '../services/user.service.js';
-import { tokenService } from '../services/token.service.js';
 
 export const musicServicesRouter = express.Router();
 
@@ -23,21 +20,22 @@ passport.use(
     },
     async (
       req: Request,
-      _accessToken: string,
-      _refreshToken: string,
+      accessToken: string,
+      refreshToken: string,
       _expires_in: number,
       profile: Profile,
       done: VerifyCallback,
     ) => {
-      const { refreshToken } = req.cookies;
-      const jwtUser = jwtService.verifyRefresh(refreshToken) as JwtPayload;
+      const { refreshToken: jwtRefreshToken } = req.cookies;
+      const jwtUser = jwtService.verifyRefresh(jwtRefreshToken) as JwtPayload;
 
       await prisma.user.update({
         where: {
           id: jwtUser.id,
         },
         data: {
-          spotifyOAUTH: profile.id,
+          spotifyOAUTH: accessToken,
+          spotifyRefresh: refreshToken,
         },
       });
 
@@ -51,13 +49,7 @@ musicServicesRouter.get('/auth/spotify', passport.authenticate('spotify'));
 musicServicesRouter.get(
   '/auth/spotify/callback',
   passport.authenticate('spotify'),
-  async (req: Request, res: Response) => {
-    const { refreshToken } = req.cookies;
-
-    const token = await tokenService.getByToken(refreshToken);
-    const user = await userService.findById(token?.userId as string);
-
+  async (_req: Request, res: Response) => {
     res.redirect(`${process.env.CLIENT_HOST as string}/tracks`);
-    // res.send(userService.normalize(user as User));
   },
 );
