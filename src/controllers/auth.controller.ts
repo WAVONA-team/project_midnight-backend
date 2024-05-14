09dcec193b25b32bb984f13ef8ffcd235cad89bf
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-14 14:39:11 +0200

    fix(TAS-386): add path

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -93,6 +93,7 @@ const generateTokens = async (res: Response, user: User) => {
     httpOnly: true,
     sameSite: 'none',
     secure: true,
+    path: '/; samesite=None; Partitioned',
   });
 
   res.send({
