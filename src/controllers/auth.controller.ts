2199712d583c4edba139ab4963f7d0037a984e5a
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-14 17:50:07 +0200

    fix(TAS-386): disable path

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -93,7 +93,7 @@ const generateTokens = async (res: Response, user: User) => {
     httpOnly: true,
     sameSite: 'none',
     secure: true,
-    path: '/; samesite=None; Partitioned',
+    // path: '/; samesite=None; Partitioned',
     domain: 'project-midnight.com',
   });
 
