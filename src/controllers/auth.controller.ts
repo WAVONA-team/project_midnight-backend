356800070585ceb4f7cb703fab569909d7e58245
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-15 12:33:52 +0200

    fix(TAS-386): disable domain

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -94,7 +94,7 @@ const generateTokens = async (res: Response, user: User) => {
     secure: true,
     httpOnly: true,
     path: '/; samesite=None; Partitioned',
-    domain: 'project-midnight.com',
+    // domain: 'project-midnight.com',
   });
 
   res.send({
